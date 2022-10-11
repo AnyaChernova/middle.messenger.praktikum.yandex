daf72bc3a8d1021893e5dfe434ae78b4160d57bd
Anna Chernova <ashubina0810@mail.ru>
2022-10-11 17:29:50 +0300

    update server js

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const app = express();
 const PORT = 3000;
 
 app.use(express.static('dist'));
+const path = require('path');
+app.get('*', (req, res) => {
+	res.sendFile(
+		path.resolve(__dirname, 'dist', 'index.html')
+	);
+});
 
 app.listen(PORT, function () {
 	console.log(`Messenger app listening on port ${PORT}!`);
