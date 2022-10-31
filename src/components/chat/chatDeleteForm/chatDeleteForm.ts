bd1cf4178242386554fee70a3559cd6705a13133
Anna Chernova <ashubina0810@mail.ru>
2022-10-31 10:26:35 +0300

    test precommit

--- src/components/chat/chatDeleteForm/chatDeleteForm.ts
+++ src/components/chat/chatDeleteForm/chatDeleteForm.ts
@@ -13,7 +13,7 @@ export class ChatDeleteForm extends Form {
 			...props,
 			button: new Button({
 				btnClass: 'w-full',
-				btnText: 'Delete'
+				btnText: 'Delete',
 			}),
 		});
 	}
@@ -22,7 +22,7 @@ export class ChatDeleteForm extends Form {
 		(this.children.button as Button).setLoading(true);
 		await store.dispatch(deleteChat, this.props.chatId);
 		await store.dispatch(setActiveChat);
-		store.dispatch({ activeModal: '' });
+		store.dispatch({ activeModal: '' })
 	}
 
 	render() {
