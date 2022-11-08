377c8a5fc2e3faa9529a5929627b791ba6373d8f
Anna Chernova <ashubina0810@mail.ru>
2022-11-08 13:16:29 +0300

    fix avatar

--- src/components/avatar/avatarUser.ts
+++ src/components/avatar/avatarUser.ts
@@ -5,7 +5,7 @@ import { Block } from '../../core/Block';
 class AvatarUserClass extends Avatar {
 	constructor(props: Indexed) {
 		super({
-			url: require('/static/avatar.svg'),
+			url: '',
 			...props,
 			size: props.size ?? 40,
 		});
@@ -13,5 +13,5 @@ class AvatarUserClass extends Avatar {
 }
 
 export const AvatarUser = withStore(AvatarUserClass as typeof Block, (state) => ({
-		url: state.avatar,
+		url: state.avatar || require('/static/avatar.svg'),
 	}));
